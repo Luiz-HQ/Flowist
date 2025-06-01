319cd446e4816c0e8e73082fe2b72a78b7c1983f
LuizDev <luizhn1703@gmail.com>
2025-06-01 02:20:41 -0300

    Fixed PrismaClient import using custom path

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/default.js";
+
+const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/signup", async (req, res) => {
+  const { email, name } = req.body;
+  const result = await prisma.user.create({
+    data: { email, name },
+  });
+  res.json(result);
 });
 
 app.listen(port, () => {
