58a6d7512f22f40b4107d51688e724babee48b87
LuizDev <luizhn1703@gmail.com>
2025-06-04 15:49:46 -0300

    Removed client and server folder on the repositorie

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-// import { signUp } from "../controllers/auth";
-
-// const Router = express.Router();
-
-// Router.post("/signup", signUp);
-
-// export default Router;
-//resolver isso e entender separação das pastas
