c17d2a8f1c6f7410f1db799543c5b05132df325b
LuizDev <luizhn1703@gmail.com>
2025-06-10 18:28:28 -0300

    Created post and get methods per user, removed duplicated prisma and unnecessary folders

---
+++ src/app/api/todo/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
+import { verifyToken } from "../middleware/verifyToken";
+
+export async function GET(req: NextRequest) {
+  try {
+    const decoded: any = verifyToken(req);
+    const userId = decoded.id;
+
+    const todos = await prisma.user.findMany({
+      where: { id: userId },
+      select: {
+        todos: {
+          orderBy: { createdAt: "asc" },
+        },
+      },
+    });
+
+    return NextResponse.json(todos, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json({ err: err.message }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const decoded: any = verifyToken(req);
+    const userId = decoded.id;
+
+    const body = await req.json();
+    const { title, description } = body;
+
+    if (!title) {
+      return NextResponse.json(
+        { message: "Títulos são obrigatórios." },
+        { status: 400 }
+      );
+    }
+
+    const newTask = await prisma.todo.create({
+      data: {
+        title,
+        description,
+        userId,
+      },
+    });
+
+    return NextResponse.json(newTask, { status: 201 });
+  } catch (err: any) {
+    return NextResponse.json({ err: err.message }, { status: 500 });
+  }
+}
